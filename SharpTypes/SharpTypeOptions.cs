674fe24eec03266b5d0985cd72699af2a564dad2
ludvigsellebraten <ludvig.sellebraten@laget.se>
2024-09-29 17:37:28 +0200

    add currentDir as option.default

--- SharpTypes/SharpTypeOptions.cs
+++ SharpTypes/SharpTypeOptions.cs
@@ -2,7 +2,7 @@ namespace SharpTypes;
 
 public class SharpTypeOptions()
 {
-    public string DefaultOutputDirectory { get; set; }
+    public string DefaultOutputDirectory { get; set; } = Directory.GetCurrentDirectory();
     public string[] Assemblies { get; set; }
     public bool GenerateIndexFile { get; set; } = false;
 }
